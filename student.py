80a4473afc5231b857d960e496a40265141ae411
Abdullah-Molapiya <abdullahdotzyx@gmail.com>
2026-07-16 12:03:05 +0530

    Initial commit with student management system and tests

---
+++ student.py
@@ -0,0 +1,15 @@
+students = {}
+
+def add_student(student_id, student_name):
+    students[student_id] = student_name
+
+def remove_student(student_id):
+    if student_id in students:
+        del students[student_id]
+
+def search_student(student_id):
+    return students.get(student_id)
+
+def update_student(student_id, student_name):
+    if student_id in students:
+        students[student_id] = student_name
---
+++ test_student.py
@@ -0,0 +1,63 @@
+from student import (students,
+add_student, remove_student, search_student, update_student)
+
+def setup_function():
+    students.clear()
+
+def test_add_student():
+    add_student(1, "Abdullah")
+    assert students[1] == "Abdullah"
+
+def test_search_student():
+    add_student(1, "Abdullah")
+    assert search_student(1) == "Abdullah"
+
+def test_search_student_not_found():
+    assert search_student(10) is None
+
+def test_remove_student():
+    add_student(1, "Abdullah")
+    remove_student(1)
+    assert search_student(1) is None
+
+def test_remove_student_not_found():  # Removing a Student That Doesn't Exist
+    remove_student(10)  # Should not raise an error
+    assert search_student(10) is None
+
+def test_update_student():
+    add_student(1, "Aryan")
+    update_student(1, "Abdullah")
+    assert search_student(1) == "Abdullah"
+
+def test_update_student_not_found():  # Update a Student That Doesn't Exist
+    update_student(10, "Zeeshan")
+    assert search_student(10) is None
+
+def test_add_multiple_students():  # Add multiple students and verify they are stored correctly
+    add_student(1, "Abdullah")
+    add_student(2, "Zeeshan")
+    add_student(3, "Aryan")
+
+    assert search_student(1) == "Abdullah"
+    assert search_student(2) == "Zeeshan"
+    assert search_student(3) == "Aryan"
+
+def test_update_one_student():
+    add_student(1, "Abdullah")
+    add_student(2, "Rahul")
+
+    update_student(2, "Zeeshan")
+
+    assert search_student(1) == "Abdullah"
+    assert search_student(2) == "Zeeshan"
+
+def test_remove_one_student():
+    add_student(1, "Abdullah")
+    add_student(2, "Zeeshan")
+
+    remove_student(2)
+
+    assert search_student(1) == "Abdullah"
+    assert search_student(2) is None
+
+
